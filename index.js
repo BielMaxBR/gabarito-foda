b0c5ec41b46f25f3ea5cff2daf473c52ae3bce50
Gabriel Maximo <gbielmaximo@gmail.com>
2021-12-04 02:47:33 +0000

    pausa pra dps

--- index.js
+++ index.js
@@ -8,9 +8,16 @@ const io = new Server({
 })
 io.on('connection', socket => {
     client.hgetall("gabarito", (err, value) => {
-        socket.emit('init', value)
+        client.hgetall("materias", (err, value2) => {
+            socket.emit('init', value, value2)
+        })
     })
     console.log('alguem conectou')
+
+    socket.on('updateAlternativa', (materia, alternativa) => {
+        client.hset("gabarito", `${materia}`, `${alternativa}`)
+        io.emit("updateAlternativa", materia, alternativa)
+    }) 
 })
 
 io.listen(process.env.PORT || 4000)
--- redis_client.js
+++ redis_client.js
@@ -11,8 +11,10 @@ const client = createClient({
 client.on('ready', () => {
     console.log('redis conectado')
     client.del("gabarito")
+    client.del("materias")
     for(let question = 1; question < 91; question++) {
         client.hset("gabarito", `${question}`, "")
+        client.hset("materias", `${question}`, "")
     }
 })
 
